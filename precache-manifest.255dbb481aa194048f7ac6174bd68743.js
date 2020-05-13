febd678ba8bea75d28ab5503769e803cd5c9c65e
umarf786 <onskadthecamel786@outlook.com>
2020-05-13 06:02:36 +0100

    Updates

---
+++ precache-manifest.255dbb481aa194048f7ac6174bd68743.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04be1d34356a61d65490a4596d01756",
+    "url": "/frontend_api/index.html"
+  },
+  {
+    "revision": "a0a2a4da4d3006f4f9ab",
+    "url": "/frontend_api/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e6ed18e4276e8a536a8c",
+    "url": "/frontend_api/static/css/main.ad4ee242.chunk.css"
+  },
+  {
+    "revision": "a0a2a4da4d3006f4f9ab",
+    "url": "/frontend_api/static/js/2.82e17383.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontend_api/static/js/2.82e17383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6ed18e4276e8a536a8c",
+    "url": "/frontend_api/static/js/main.f956769a.chunk.js"
+  },
+  {
+    "revision": "d4344a0c58de75a25738",
+    "url": "/frontend_api/static/js/runtime-main.37d61159.js"
+  }
+]);
